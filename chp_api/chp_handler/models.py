163760dc0e60315786fa615872becff5c9b55182
veenhouse <Luke.J.Veenhuis@dartmouth.edu>
2020-09-21 12:37:04 -0400

    integrated postgress instead of mongodb

--- chp_api/chp_handler/models.py
+++ chp_api/chp_handler/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 # Create your models here.
 
+class Transaction(models.Model):
+    date_time = models.DateTimeField(auto_now=True)
+    source_ara = models.CharField(max_length=50)
+
+    query = models.JSONField()
+    response = models.JSONField()
+    
+    english_translation = models.TextField()
