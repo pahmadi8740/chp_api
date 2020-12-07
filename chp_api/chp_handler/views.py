1c01beda73b36da8f453f01d906f3038666fc99e
Chase Yakaboski <chase.a.yakaboski@dartmouth.edu>
2020-12-07 01:57:44 -0500

    Saving work, new view using Trapi interface

--- chp_api/chp_handler/views.py
+++ chp_api/chp_handler/views.py
@@ -16,9 +16,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from chp.reasoner_std import ReasonerStdHandler
+from chp.trapi_nterface import TrapiInterface
 
-#-- Get Hosts File if it exists
+# Get Hosts File if it exists
 parent_dir = os.path.dirname(os.path.realpath(__file__))
 HOSTS_FILENAME = os.path.join(parent_dir, 'hosts')
 NUM_PROCESSES_PER_HOST = multiprocessing.cpu_count()
@@ -26,78 +26,83 @@ NUM_PROCESSES_PER_HOST = multiprocessing.cpu_count()
 HOSTS_FILENAME = None
 NUM_PROCESSES_PER_HOST = 0
 
-
-#import the transaction model so that we may store queries and responses
+# Import the transaction model so that we may store queries and responses
 from chp_handler.models import Transaction
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def _process_request(request):
+    logger.info('Starting query.')
+    data = request.data
+    query = data.pop('message', None)
+    max_results = data.pop('max_results', 10)
+    client_id = data.pop('client_id', 'default')
+    return query, max_results, client_id
+
+def _get_response_to_query(query):
+        start_time = time.time()
+
+        # Instaniate TRAPI Interface
+        interface = TrapiInterface(
+            query=query,
+            hosts_filename=HOSTS_FILENAME,
+            num_processes_per_host=NUM_PROCESSES_PER_HOST,
+            max_results=max_results,
+            client_id=client_id,
+        )
+
+        # Build queries
+        handler.buildChpQueries()
+        logger.info('Built Queries.')
+        # Run queries
+        handler.runChpQueries()
+        logger.info('Completed Reasoning in {} seconds.'.format(time.time() - start_time))
+
+        # Construct Response
+        response = handler.constructDecoratedKG()
+
+        #TODO: Cache answer.
+
+        return JsonResponse(response)
+
+class query_all(APIView):
+
+    def post(self, request):
+        if request.method == 'POST':
+            query, max_results, client_id = _process_request(request)
+            return _get_response_to_query(query, max_results, client_id)
 
 class query(APIView):
 
     def post(self, request):
         if request.method == 'POST':
-            start_time = time.time()
-            data = request.data
-
-            query = data['message']
-            max_results = data.pop('max_results', 10)
-
-            if 'reasoner_id' in query.keys():
-                source_ara = query['reasoner_id']
-            else:
-                source_ara = 'default'
-
-            print('Processing query from: {}'.format(source_ara))
-
-            handler = ReasonerStdHandler(source_ara,
-                                         dict_query=query,
-                                         hosts_filename=HOSTS_FILENAME,
-                                         num_processes_per_host=NUM_PROCESSES_PER_HOST,
-                                         max_results=max_results)
-            handler.buildChpQueries()
-            print('Built Queries.')
-            handler.runChpQueries()
-            print('Completed Reasoning.')
-            print('Total Time: {}'.format(time.time() - start_time))
-
-            response = handler.constructDecoratedKG()
-
-            #-- This is block is currenlty not working.
-            #Store the transaction in mongodb
-            #transaction = Transaction()
-            #transaction.source_ara = source_ara
-            #transaction.query = query
-            #transaction.response = response
-
-            #transaction.save()
-
-            return JsonResponse(response)
+            query, max_results, client_id = _process_request(request)
+            return _get_response_to_query(query, max_results, client_id)
 
 class check_query(APIView):
 
     def post(self, request):
         if request.method == 'POST':
-            data = request.data
+            query, max_results, client_id = _process_request(request)
 
-            query = data['query']
-            source_ara = query['reasoner_id']
+            # Instaniate interface
+            interface = TrapiInterface(query=query, client_id=client_id)
 
-            handler = ReasonerStdHandler(source_ara, dict_query=query)
-
-            return JsonResponse(handler.checkQuery())
+            return JsonResponse(interface.check_query())
 
 class curies(APIView):
     
     def get(self, request):
         if request.method == 'GET':
-            data = request.data
+            query, max_results, client_id = _process_request(request)
 
-            if 'reasoner_id' in data.keys():
-                source_ara = data['reasoner_id']
-            else:
-                source_ara = 'default'
+            # Instaniate interface
+            interface = TrapiInteadsrface(client_id=client_id)
 
-            # Get the handler with no query
-            handler = ReasonerStdHandler(source_ara)
-            curies = handler.getCuries()
+            # Get supported curies
+            curies = interface.get_curies()
 
             return JsonResponse(curies)
 
@@ -105,6 +110,13 @@ class predicates(APIView):
     
     def get(self, request):
         if request.method == 'GET':
+            query, max_results, client_id = _process_request(request)
+
+            # Instaniate interface
+            interface = TrapiInterface(client_id=client_id)
+
+            # Get supported predicates
+            predicates = interface.get_predicates()
             predicate_map = {
                               'biolink:Gene' : {
                                                 'biolink:Disease' : ['biolink:GeneToDiseaseAssociation']
@@ -117,7 +129,7 @@ class predicates(APIView):
                                                    'biolink:PhenotypicFeature' : ['biolink:DiseaseToPhenotypicFeatureAssociation']
                                                   }
                             }
-            return JsonResponse(predicate_map)
+            return JsonResponse(predicates)
 
 class versions(APIView):
 
