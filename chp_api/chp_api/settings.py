646e78b4b96163a9940f150c163d94ec5b380649
veenhouse <luke.veenhuis@gmail.com>
2021-01-13 15:45:30 -0600

    removing cors support

--- chp_api/chp_api/settings.py
+++ chp_api/chp_api/settings.py
@@ -38,6 +38,10 @@ REST_FRAMEWORK = {
 
 # Application definition
 
+CORS_ALLOWED_ORIGINS = [
+    "https://smart-api.info/"
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,10 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'chp_handler'
+    'chp_handler',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
