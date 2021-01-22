92497a94a12961d9fa84312353bbc2bf63d86499
veenhouse <luke.veenhuis@gmail.com>
2021-01-21 21:48:16 -0600

    setting CORS configuration in settings.py

--- chp_api/chp_api/settings.py
+++ chp_api/chp_api/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'chp_handler'
+    'chp_handler',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'chp_api.urls'
 
