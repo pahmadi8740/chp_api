075c8380538891cdf77263b9427c13208470868d
veenhouse <luke.veenhuis@gmail.com>
2021-07-22 16:42:03 -0500

    fixed error handling

--- chp_api/chp_handler/views.py
+++ chp_api/chp_handler/views.py
@@ -31,11 +31,14 @@ class query_all(APIView):
                 data_copy = deepcopy(request.data)
                 query_processor = QueryProcessor(request, self.trapi_version)
             except UnsupportedPrefix as e:
-                response_dict = data_copy
-                response_dict['status'] = 'Bad request.' + str(e)
-                return JsonResponse(response_dict, status=400) 
+                response = { 'query_graph' : data_copy,
+                     'knowledge_graph' : { 'edges': dict(), 'nodes': dict()},
+                     'results': [] }
+                message = {'message' : response,
+                        'description' : 'Unsupported query',
+                        'status': 'Bad Request. ' + str(e)}
             except ValidationError as e:
-                response = { 'query_graph' : self.query,
+                response = { 'query_graph' : data_copy,
                      'knowledge_graph' : { 'edges': dict(), 'nodes': dict()},
                      'results': [] }
                 message = {'message' : response,
@@ -43,9 +46,12 @@ class query_all(APIView):
                         'status': 'Bad Request. ' + str(e)}
                 return JsonResponse(message, status=400)
             except Exception as e:
-                response_dict = data_copy
-                response_dict['status'] = 'Bad request.' + str(e)
-                return JsonResponse(response_dict) 
+                response = { 'query_graph' : data_copy,
+                     'knowledge_graph' : { 'edges': dict(), 'nodes': dict()},
+                     'results': [] }
+                message = {'message' : response,
+                        'description' : 'Unsupported query',
+                        'status': 'Bad Request. ' + str(e)} 
             return query_processor.get_response_to_query()
 
 class query(APIView):
@@ -60,13 +66,21 @@ class query(APIView):
                 data_copy = deepcopy(request.data)
                 query_processor = QueryProcessor(request, self.trapi_version)
             except UnsupportedPrefix as e:
-                response_dict = data_copy
-                response_dict['status'] = 'Bad request.' + str(e)
-                return JsonResponse(response_dict, status=400) 
+                response = { 'query_graph' : data_copy,
+                     'knowledge_graph' : { 'edges': dict(), 'nodes': dict()},
+                     'results': [] }
+                message = {'message' : response,
+                        'description' : 'Unsupported query',
+                        'status': 'Bad Request. ' + str(e)}
+                return JsonResponse(message, status=400) 
             except Exception as e:
-                response_dict = data_copy
-                response_dict['status'] = 'Bad request.' + str(e)
-                return JsonResponse(response_dict) 
+                response = { 'query_graph' : data_copy,
+                     'knowledge_graph' : { 'edges': dict(), 'nodes': dict()},
+                     'results': [] }
+                message = {'message' : response,
+                        'description' : 'Unsupported query',
+                        'status': 'Bad Request. ' + str(e)}
+                return JsonResponse(message) 
             return query_processor.get_response_to_query()
 
 class check_query(APIView):
