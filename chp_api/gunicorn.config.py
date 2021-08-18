a81533471bce65d83391aaea7e90bd8da721bed4
Chase Yakaboski <chase.th@dartmouth.edu>
2021-08-18 15:08:52 -0400

    Added more workers and edited gunicorn config files and docker compose for prod.

--- chp_api/gunicorn.config.py
+++ chp_api/gunicorn.config.py
@@ -6,3 +6,8 @@ limit_request_field_size  = 0
 limit_request_line = 0
 limit_request_fields = 0
 proxy_allow_ips = '*'
+workers=3
+errorlog='gunicorn-error.log'
+accesslog='gunicorn-access.log'
+loglevel='debug'
+
