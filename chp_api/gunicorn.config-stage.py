61dfc8b9609460e3c5edd449d9db60ad80e1453f
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2021-09-09 14:47:02 -0400

    Differentiate between gunicorn config files. Setting up stage branches will now only launch 1 worker and should be better on memory for loading the BKBs. Setting up prod branches will still launch 3 workers as prod sould have more memory to spare.

---
+++ chp_api/gunicorn.config-stage.py
@@ -0,0 +1,13 @@
+### Gunicorn Configuration File ###
+
+timeout = 0
+graceful_timeout = 0
+limit_request_field_size  = 0
+limit_request_line = 0
+limit_request_fields = 0
+proxy_allow_ips = '*'
+workers=3
+errorlog='gunicorn-error.log'
+accesslog='gunicorn-access.log'
+loglevel='debug'
+
