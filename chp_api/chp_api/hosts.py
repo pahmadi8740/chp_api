bd6294d8095b2dd67f1ebc6cd1b90ce05f6a2e8f
Chase Yakaboski <chase.a.yakaboski@dartmouth.edu>
2021-09-23 08:11:06 -0400

    Working to server load. Haven't tested chp app yet. Likely bugs!

--- chp_api/chp_api/hosts.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-from django_hosts import patterns, host
-
-host_patterns = patterns('',
-        host(r'www', settings.ROOT_URLCONF, name='www'),
-        host(r'(breast|brain|lung)', 'apis.chp_core.urls', name='chp-core'),
-        )
