861d88ad30ee64ce4be67d2dce55891f348534c7
Ojesanmi <ojesanmiaa@nih.gov>
2023-01-06 16:28:00 -0500

    fix: modified deploy.sh file

--- deploy/chp-api/deploy.sh
+++ deploy/chp-api/deploy.sh
@@ -23,12 +23,10 @@ do
 done
 
 sed -i.bak \
-    -e "s/SECRET_KEY_VALUE/$SECRET_KEY/g" \
-    -e "s/ENGINE_VALUE/$ENGINE/g;s/DBNAME_VALUE/$DBNAME/g" \
-    -e "s/USERNAME_VALUE/$USERNAME/g;s/PASSWORD_VALUE/$PASSWORD/g" \
-    -e "s/HOST_VALUE/$HOST/g;s/PORT_VALUE/$PORT/g" \
-    configs/settings.py
-rm configs/settings.py.bak 
+    -e "s/APP_SECRET_KEY_VALUE/$APP_SECRET_KEY/g;s/DB_DATABASE_VALUE/$DB_DATABASE/g" \
+    -e "s/DB_USERNAME_VALUE/$DB_USERNAME/g;s/DB_PASSWORD_VALUE/$DB_PASSWORD/g" \
+    values-ncats.yaml
+rm values-ncats.yaml.bak 
 
 kubectl apply -f namespace.yaml
 
