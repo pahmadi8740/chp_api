9cb0d6682618bd7d597ad47914e5dc7f2f288b0e
Ojesanmi <ojesanmiaa@nih.gov>
2023-01-06 14:26:58 -0500

    fix: refactor chp ci pipeline

--- deploy/chp-api/deploy.sh
+++ deploy/chp-api/deploy.sh
@@ -22,6 +22,14 @@ do
   rm values.yaml.bak
 done
 
+sed -i.bak \
+    -e "s/SECRET_KEY_VALUE/$SECRET_KEY/g" \
+    -e "s/ENGINE_VALUE/$ENGINE/g;s/DBNAME_VALUE/$DBNAME/g" \
+    -e "s/USERNAME_VALUE/$USERNAME/g;s/PASSWORD_VALUE/$PASSWORD/g" \
+    -e "s/HOST_VALUE/$HOST/g;s/PORT_VALUE/$PORT/g" \
+    configs/settings.py
+rm configs/settings.py.bak 
+
 kubectl apply -f namespace.yaml
 
 # deploy helm chart
